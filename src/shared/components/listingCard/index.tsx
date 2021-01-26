45f31055ea978e6e01064e45bdcbf7bd0ce8efd3
eabrk <burak.altinyazar@ug.bilkent.edu.tr>
2021-01-26 17:35:57 +0300

    Listing Card Iteration#1
    Listing Card Componend added to Shared Components

---
+++ src/shared/components/listingCard/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Button from '../button'
+import SimpleText from '../text/simple-text'
+import styles from './styles'
+interface ListingCardProps {
+
+    image: string,
+    data: JSON
+    //things
+
+}
+// use with a container with such styles
+// style={{ display: "flex", flexDirection: "row" }}
+function ListingCard(props: ListingCardProps): JSX.Element {
+
+    return (
+        <div style={styles.card}>
+            <img src={props.image} style={styles.image} />
+            <SimpleText textID="prop1 Data Json Text" additionalStyle={styles.text} />
+            <SimpleText textID="prop2 Data Json Text" additionalStyle={styles.text} />
+            <SimpleText textID="prop3 Data Json Text" additionalStyle={styles.text} />
+            <SimpleText textID="prop4 Data Json Text" additionalStyle={styles.text} />
+            <SimpleText textID="prop5 Data Json Text" additionalStyle={styles.text} />
+            <div style={styles.buttonContainer}>
+                <Button onPress={() => console.log('1')}>Button Text1</Button>
+                <Button onPress={() => console.log('1')}>Button Text2</Button>
+                <Button onPress={() => console.log('1')}>Button Text3</Button>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default ListingCard
---
+++ src/shared/components/listingCard/styles.ts
@@ -0,0 +1,21 @@
+export default {
+  card: {
+    height: "400",
+    width: "400",
+    borderStyle: "solid",
+    margin: "5",
+    padding: "5",
+  },
+  image: {
+    width: "100%",
+  },
+  text: {
+    fontSize: 18,
+    color: "black",
+  },
+  buttonContainer: {
+    paddingTop: "20",
+    display: "flex",
+    justifyContent: "space-around",
+  },
+};
