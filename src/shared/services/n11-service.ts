5a0d523b7528666777ec2f3364558ad57dac90d4
leylaHashimliii <leyla.hashimli@ug.bilkent.edu.tr>
2021-03-17 16:26:44 +0300

    authentication added

--- src/shared/services/n11-service.ts
+++ src/shared/services/n11-service.ts
@@ -1,13 +1,28 @@
 import { Integration } from "../models/Integration";
+import GetProductByProductIdRequest from "n11-client";
+import Authentication from "n11-client";
 	
 class N11Integration {
+     
+    strAppKey: string;
+    strAppSecret: string;
 
-    constructor() {
+    constructor() 
+    {
+        const authentication = new Authentication();
+        authentication.setAppKey(this.strAppKey);
+        authentication.setAppSecret(this.strAppSecret);
+    }
+    getProductInfoWithProductID(productID: string): string
+    {
+        return ""
         
     }
+    
 
     getIntegrations(): Integration[] {
         // implement here
+
         return []
     }
 }
