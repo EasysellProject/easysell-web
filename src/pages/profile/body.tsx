17b9882079b277cfd4ef48d5c523566fbcfc0100
leylaIsmayilova <leyla.ismayilova@ug.bilkent.edu.tr>
2021-03-17 14:35:27 +0300

    profil updated

--- src/pages/profile/body.tsx
+++ src/pages/profile/body.tsx
@@ -6,11 +6,22 @@ import styles from "./styles";
 import {FaPen} from "react-icons/fa";
 import { APP_COLORS, WEB_STYLES } from '../../shared/styles';
 
+
 interface BodyProps {
 
 }
 
 function Body(props: BodyProps): JSX.Element {
+
+    function validateEmail( email){
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+        if (!pattern.test(email) ){
+            var a;
+          //print();//errors["email"] = "Please enter valid email address.";
+
+        }
+    }
     function onSaveChanges(): void {
         // TODO implement
     }
@@ -21,7 +32,7 @@ function Body(props: BodyProps): JSX.Element {
             ...WEB_STYLES.flexWrap,
             marginLeft: "5%",
             marginTop: "5%",
-            height: "60%",
+            height: "50%",
             width: "100%",
             justifyContent: "space-between",
             alignItems: "flex-start",
@@ -66,26 +77,7 @@ function Body(props: BodyProps): JSX.Element {
                 />
                 <FaPen color = {APP_COLORS.ligthBlue} style={{marginLeft: 10, marginTop: 15}} />
                 </div>
-                <div style={{
-                display: "flex",
-            ...WEB_STYLES.flexRow,
-            alignItems:"center",}}>
-                <Input
-                    value=""
-                    placeholder="Birthdate"
-                    showLabel
-                    required
-                    label="Birthdate"
-                    onChangeText={(name: string) => {
-                        //resetErrors()
-                        //setName(name)
-                    }}
-                    //additionalStyles={styles.inputContainer}
-                    //showError={nameError}
-                    errorText={'error-empty-field'}
-                />
-                <FaPen color = {APP_COLORS.ligthBlue} style={{marginLeft: 10, marginTop: 15}} />
-                </div>
+                
                 <div style={{
                 display: "flex",
             ...WEB_STYLES.flexRow,
