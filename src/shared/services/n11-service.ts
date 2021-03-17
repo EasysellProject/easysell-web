9a682e6532c6f2a4624fd18e935a45d0ed19fb14
leylaIsmayilova <leyla.ismayilova@ug.bilkent.edu.tr>
2021-03-17 15:49:59 +0300

    service added

---
+++ src/shared/services/n11-service.ts
@@ -0,0 +1,15 @@
+import { Integration } from "../models/Integration";
+	
+class N11Integration {
+
+    constructor() {
+        
+    }
+
+    getIntegrations(): Integration[] {
+        // implement here
+        return []
+    }
+}
+
+export default new N11Integration();
