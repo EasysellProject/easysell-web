b46daf007c65a0dc5ce06e0093efedc226323116
leylaHashimliii <leyla.hashimli@ug.bilkent.edu.tr>
2021-04-12 21:53:39 +0300

    randomListingGenerator function created

--- src/shared/services/auth-service.ts
+++ src/shared/services/auth-service.ts
@@ -1,22 +1,24 @@
 import { User } from "../models/user";
+import { Listing } from "../models/listing"
 import firebase from "../utils/firebase";
 import { LangCode } from "../utils/localization";
 import UserService from "./user-service";
+import { RiLandscapeFill, RiLayout5Fill } from "react-icons/ri";
 class AuthService {
-
   async register(email: string, password: string, firstname: string, lastname: string, lang: LangCode) {
     try {
       let res = await firebase.auth().createUserWithEmailAndPassword(email, password)
-      console.log('res ', res)
+      let randomListings = this.generateRandomListings()
       //TODO save user to firestore
       let currentUser = {
+        _id: res.user?.uid,
         firstname,
         lastname,
         lang,
         email,
-        _id: res.user?.uid
-      }
-      await firebase.firestore().collection('users').doc(currentUser._id).set({ firstname, lastname, lang, email, _id: res.user?.uid })
+      } 
+      await firebase.firestore().collection('users').doc(currentUser._id).set(currentUser)
+      
       UserService.currentUser = new User(currentUser)
       return { result: "success" }
     } catch (err) {
@@ -24,39 +26,6 @@ class AuthService {
       throw err
     }
   }
-  async createUser(
-    email: string,
-    password: string,
-    firstname: string,
-    lastname: string,
-    lang: LangCode
-  ) {
-    try {
-      let res = await firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password);
-      console.log("res ", res);
-      //TODO save user to firestore
-      let currentUser = {
-        firstname,
-        lastname,
-        lang,
-        email,
-        _id: res.user?.uid,
-      };
-      await firebase
-        .firestore()
-        .collection("users")
-        .doc(currentUser._id)
-        .set({ firstname, lastname, lang, email, _id: res.user?.uid });
-      UserService.currentUser = new User(currentUser);
-      return { result: "success" };
-    } catch (err) {
-      console.log("err ", err);
-      throw err;
-    }
-  }
-
   async signIn(email: string, password: string) {
     try {
       let res = await firebase.auth().signInWithEmailAndPassword(email, password)
@@ -87,6 +56,83 @@ class AuthService {
       throw err;
     }
   }
+  generateRandomListings(): Listing[] {
+    let listings: Listing[] = [];
+    let infos = [
+      {
+        title: "Nike",
+        desc: "Unisex Küçük Boy Sırt Çantası",
+        img: 'https://cdn.dsmcdn.com/mnresize/-/-//ty76/product/media/images/20210222/19/66004015/144844476/1/1_org_thumb.jpg',
+        price: 145,
+        currency: "TL"
+      }
+      ,
+      {
+        title: "Nike",
+        desc: "Nike 2.0 elementi Backpack Sırt Çantası",
+        img: 'https://cdn.dsmcdn.com/mnresize/-/-//ty45/product/media/images/20210105/9/46370072/99054131/1/1_org_thumb.jpg',
+        price: 140,
+        currency: "TL"
+      },
+      {
+        title: "Nike",
+        desc: "Unisex Siyah Kırmızı Detaylı Sırt Çantası",
+        img: "https://cdn.dsmcdn.com/mnresize/-/-//ty84/product/media/images/20210311/15/70718479/22374021/1/1_org_thumb.jpg",
+        price: 129.99,
+        currency: "TL"
+      },
+      {
+        title: "XS Max",
+        desc: "Iphone Tüm Cihazlarla Uyumlu Şarj Aleti",
+        img: "https://cdn.dsmcdn.com/mnresize/-/-//ty81/product/media/images/20210310/18/70501445/94101997/1/1_org_thumb.jpg",
+        price: 34.99,
+        currency: "TL"
+      },
+      {
+        title: "Nike",
+        desc: "Unisex Siyah Kırmızı Detaylı Sırt Çantası",
+        img: 'https://cdn.dsmcdn.com/mnresize/-/-//ty81/product/media/images/20210310/18/70501445/94101997/1/1_org_thumb.jpg',
+        price: 128.70,
+        currency: "TL"
+      }
+    ]
+    var rndmNUmber = Math.floor( Math.random() * 1000 + 1)
+    for (let i = 0; i < rndmNUmber; i++) 
+    {
+      let info = infos[i % infos.length];
+      let details = {
+        _id: this.generateRandomID(),
+        index: i + 1,
+        title: info.title,
+        desc: info.desc,
+        price: info.price,
+        img: info.img,
+        stock: Math.floor(Math.random() * 50),
+        marketPlace: Math.floor(Math.random() * 2) == 1 ? ['Trendyol', 'Hepsiburada'] : Math.floor(Math.random() * 2) == 1 ? ["Hepsiburada"] : ["Trendyol"],
+        currency: info.currency,
+        createdAt: this.getRandomDate(new Date(1609460000000), new Date()) // random time since 1 january 2021
+      }
+      listings.push(new Listing(details));
+    }
+
+    return listings
+  }
+
+  generateRandomID(): string {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 12; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+  getRandomDate(from: Date, to: Date): Date {
+    const fromTime = from.getTime();
+    const toTime = to.getTime();
+    return new Date(fromTime + Math.random() * (toTime - fromTime));
+  }
+
 }
 
 export default new AuthService();
