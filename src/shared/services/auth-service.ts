b7308a21c04788236cb754459c3520c357f1192f
leylaIsmayilova <leyla.ismayilova@ug.bilkent.edu.tr>
2021-04-15 22:06:31 +0300

    order fetching script is added

--- src/shared/services/auth-service.ts
+++ src/shared/services/auth-service.ts
@@ -3,6 +3,7 @@ import firebase from '../utils/firebase'
 import { LangCode } from '../utils/localization'
 import UserService from './user-service'
 import { Listing } from '../models/listing'
+import { Order } from '../models/order'
 
 
 class AuthService {
@@ -130,6 +131,103 @@ class AuthService {
     return listings
   }
 
+  generateRandomOrderList(): Order[] {
+    let orders: Order[] = [];
+    let infos = [
+      {
+        title: "Nike",
+        desc: "Unisex Küçük Boy Sırt Çantası",
+        img: 'https://cdn.dsmcdn.com/mnresize/-/-//ty76/product/media/images/20210222/19/66004015/144844476/1/1_org_thumb.jpg',
+        price: 145,
+        currency: "TL"
+      }
+      ,
+      {
+        title: "Nike",
+        desc: "Nike 2.0 elementi Backpack Sırt Çantası",
+        img: 'https://cdn.dsmcdn.com/mnresize/-/-//ty45/product/media/images/20210105/9/46370072/99054131/1/1_org_thumb.jpg',
+        price: 140,
+        currency: "TL"
+      },
+      {
+        title: "Nike",
+        desc: "Unisex Siyah Kırmızı Detaylı Sırt Çantası",
+        img: "https://cdn.dsmcdn.com/mnresize/-/-//ty84/product/media/images/20210311/15/70718479/22374021/1/1_org_thumb.jpg",
+        price: 129.99,
+        currency: "TL"
+      },
+      {
+        title: "XS Max",
+        desc: "Iphone Tüm Cihazlarla Uyumlu Şarj Aleti",
+        img: "https://cdn.dsmcdn.com/mnresize/-/-//ty81/product/media/images/20210310/18/70501445/94101997/1/1_org_thumb.jpg",
+        price: 34.99,
+        currency: "TL"
+      },
+      {
+        title: "Nike",
+        desc: "Unisex Siyah Kırmızı Detaylı Sırt Çantası",
+        img: 'https://cdn.dsmcdn.com/mnresize/-/-//ty81/product/media/images/20210310/18/70501445/94101997/1/1_org_thumb.jpg',
+        price: 128.70,
+        currency: "TL"
+      }
+    ]
+
+    let cargos = [
+      {
+        _id: "0";
+        name: "Aras Kargo Firmasi";
+        shortName: "ARAS";
+        logo: "";
+        trackUrl?: "";
+      },
+      {
+        _id: "0";
+        name: "Aras Kargo Firmasi";
+        shortName: "ARAS";
+        logo: "";
+        trackUrl?: "";
+      }
+    
+    ]
+
+    var orderCount = Math.floor(Math.random() * 1000 + 1);
+    let randomOrderDate;
+    let randomDueDate;
+    let randomDay;
+
+    for (let i = 0; i < orderCount; i++) {
+      let info = infos[i % infos.length];
+      randomDay = Math.random()*29 + 1;
+      randomOrderDate = new Date( 2021, randomDay , 4);
+      if( randomDay > 23 ){
+        randomDueDate = new Date( 2021, randomDay + 7 - 30 , 5);
+      }
+      else{
+        randomDueDate = new Date( 2021, randomDay + 7 , 4);
+      }
+      let details = {
+        orderNo: this.generateRandomID(),
+        orderDate: randomOrderDate,
+        dueDate: randomDueDate,
+        orderedBy: info.desc,
+        price: info.price,
+        img: info.img,
+        stock: Math.floor(Math.random() * 50),
+        marketPlace: Math.floor(Math.random() * 2) == 1 ? ['Trendyol', 'Hepsiburada'] : Math.floor(Math.random() * 2) == 1 ? ["Hepsiburada"] : ["Trendyol"],
+        currency: info.currency,
+        createdAt: this.getRandomDate(new Date(1609460000000), new Date()) // random time since 1 january 2021
+      }
+
+        this.orderedBy = details.orderedBy;
+        this.cargoCompany = details.cargoCompany;
+        this.items = details.items.map(i => new Product(i));
+        this.deliveryType = "details.deliveryType";
+        this.deliveryAddress = details.deliveryAddress;
+      orders.push(new Order(details));
+    }
+
+    return orders
+  }
   generateRandomID(): string {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
