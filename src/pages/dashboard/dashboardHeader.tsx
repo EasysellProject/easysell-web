3a85bcb628e4ac3e8caa278fc6c99b6c55f2721b
AnarHuseynov00 <anar.huseynov@ug.bilkent.edu.tr>
2021-04-21 17:58:26 +0300

    dashboard header component has been created

---
+++ src/pages/dashboard/dashboardHeader.tsx
@@ -0,0 +1,113 @@
+import React from "react"
+import styles from './styles';
+import SimpleText from "../../shared/components/text/simple-text";
+import {BiLira} from 'react-icons/bi'
+import {FiPercent} from "react-icons/fi"
+import {RiTShirtLine} from "react-icons/ri"
+
+interface dashboardHeaderProps{
+    username:string,
+    sales_amount:number,
+    total_profit:number,
+    avg_sale_price:number,
+    profit_margin:number,
+    avg_product_price:number
+}
+
+function DashboardHeader(props:dashboardHeaderProps): JSX.Element {
+    const {username, sales_amount, total_profit, avg_sale_price, profit_margin, avg_product_price} = props
+    const date = new Date();
+    return(
+        <div style={styles.dashboardHeader}>
+            <div style={styles.firstLayer}>
+                <div style={styles.welcomeLayer}>
+                    <SimpleText textID={"welcome"}
+                        additionalStyle={styles.headerText}/>
+                    <SimpleText textID={username}
+                        additionalStyle={styles.nameText}/>
+                </div>
+                <div>                
+                    <SimpleText textID={date.toLocaleDateString("en-US", {weekday:"long", year:"numeric", month:"long", day:"numeric"})}
+                        additionalStyle={styles.dateText}/>
+                </div>
+            </div>
+            <div style={styles.numbersLayer}>
+                <div style={styles.infoLayer}>
+                    <div style={styles.infoLayerTextArea}>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerText}
+                        textID={"sales-amount"}/>
+                        <div style={{display:"flex", flexDirection:"row"}}>
+                        <BiLira size={15} color="green" style={{marginTop:3}}/>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerTextNumber}
+                        textID={sales_amount.toString()}/>
+                        </div>
+                    </div>
+                    <div style={styles.infoLayerSymbolLira}>
+                        <BiLira/>
+                    </div>
+                </div>
+                <div style={styles.infoLayer}>
+                <div style={styles.infoLayerTextArea}>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerText}
+                        textID={"gross-profit"}/>
+                        <div style={{display:"flex", flexDirection:"row"}}>
+                            <BiLira size={15} color="green" style={{marginTop:3}}/>
+                            <SimpleText
+                            additionalStyle={styles.infoLayerTextNumber}
+                            textID={sales_amount.toString()}/>
+                        </div>
+                    </div>
+                    <div style={styles.infoLayerSymbolPercent}>
+                        <FiPercent/>
+                    </div>
+                </div>
+                 <div style={styles.infoLayer}>
+                 <div style={styles.infoLayerTextArea}>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerText}
+                        textID={"avg-sale-price"}/>
+                        <div style={{display:"flex", flexDirection:"row"}}>
+                            <BiLira size={15} color="green" style={{marginTop:3}}/>
+                            <SimpleText
+                            additionalStyle={styles.infoLayerTextNumber}
+                            textID={sales_amount.toString()}/>
+                        </div>
+                    </div>
+                </div>
+                <div style={styles.infoLayer}>
+                <div style={styles.infoLayerTextArea}>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerText}
+                        textID={"profit-margin"}/>
+                        <div style={{display:"flex", flexDirection:"row"}}>
+                        <FiPercent size={15} color="green" style={{marginTop:3}}/>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerTextNumber}
+                        textID={sales_amount.toString()}/>
+                        </div>
+                    </div>
+                    <div style={styles.infoLayerSymbolMargin}>
+                        <RiTShirtLine/>
+                    </div>
+                </div>
+                <div style={styles.infoLayer}>
+                <div style={styles.infoLayerTextArea}>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerText}
+                        textID={"avg-product-price"}/>
+                        <div style={{display:"flex", flexDirection:"row"}}>
+                        <BiLira size={15} color="green" style={{marginTop:3}}/>
+                        <SimpleText
+                        additionalStyle={styles.infoLayerTextNumber}
+                        textID={sales_amount.toString()}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>)
+}
+
+export default DashboardHeader;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import DashboardLayout from '../../shared/components/dashboard-layout';
 import { Product } from '../../shared/models/product';
 import styles from './styles';
+import DashboardHeader from "./dashboardHeader"
 
 interface DashboardProps {
 
@@ -16,10 +17,11 @@ function Dashboard(props: DashboardProps): JSX.Element {
     return (
         <DashboardLayout route='Dashboard'>
             <div style={styles.container}>
-                Dashboard
+                <DashboardHeader username={"Anar"} sales_amount = {200} total_profit = {100} 
+                avg_sale_price = {100} profit_margin = {500} avg_product_price = {100} />
             </div>
         </DashboardLayout>
     )
 }
 
-export default Dashboard
+export default Dashboard;
